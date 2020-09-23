666769f83a4b44caf40be430eb2020dd3123e0aa
Andrew Plank <aplank14@gmail.com>
2020-09-23 18:19:58 -0400

    Added rows to communication

--- src/components/Communication/Communication.jsx
+++ src/components/Communication/Communication.jsx
@@ -10,6 +10,24 @@ const Communication = () => {
       <h1>
         {isAuthenticated ? 'Lets chat!' : 'You must login first'}
       </h1>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-4 bg-Primary">
+            <div className="">
+              <h4>Groups</h4>
+            </div>
+            <div>
+              <h4>Members</h4>
+            </div>
+          </div>
+          <div className="col-8">
+            <div>
+              <h4>Group Name</h4>
+              <span>Group Desc</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
