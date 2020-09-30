c9f6e205a19a2c9708b4ff856dd9f6e525b65dd3
Asionnn <collin.li0499@gmail.com>
2020-09-29 20:47:07 -0400

    memebers displaying nicely and toggles in

--- src/components/TeamManagement/TeamManagement.jsx
+++ src/components/TeamManagement/TeamManagement.jsx
@@ -1,59 +1,116 @@
-/* eslint-disable no-undef */
-/* eslint-disable react/no-this-in-sfc */
+/* eslint-disable */
 import React from 'react';
 import './TeamManagement.scss';
 import './TeamManagement.css';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import IconButton from '@material-ui/core/IconButton';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Grid from '@material-ui/core/Grid';
+import Switch from '@material-ui/core/Switch';
 import {
   Container,
   Row,
   Col,
-  Button,
 } from 'react-bootstrap';
+
+const members = [
+  { userID: 1, name: 'Collin' },
+  { userID: 2, name: 'Andy' },
+  { userID: 3, name: 'Jon' },
+  { userID: 4, name: 'Ray' },
+  { userID: 5, name: 'Trevor' },
+];
 
 const headerStyle = {
   textAlign: 'center',
   paddingBottom: '3%',
 };
 
-const users = ['collin', 'Andy', 'Jon', 'Ray', 'Trevor'];
+class TeamManagement extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checkedA: true,
+      adminChecked: [],
+    };
+    this.handleChange = this.handleChange.bind(this)
+  }
 
-const renderUsers = () => (
-  <>
-    {users.map((name, index) => {
-      const keyNo = index;
-      return (
-        <>
-          <Button className="zoom" key={keyNo}>
-            {name}
-          </Button>
-          <br />
-        </>
-      );
-    })}
-  </>
-);
+componentDidMount() {
+  this.setState({adminChecked: Array(members.length).fill(false)});
+}
 
-const TeamManagement = () => (
-  <>
-    <div>
-      <h1 style={headerStyle}>Team Management</h1>
-    </div>
-    <Container fluid>
-      <Row>
-        <Col xs={6} md={4}>
-          <h2>Team Controls</h2>
-          <ul>
-            <li>Create Team</li>
-            <li>Edit Team</li>
-          </ul>
-        </Col>
-        <Col xs={12} md={8}>
-          <h2>Users</h2>
-          {renderUsers()}
-        </Col>
-      </Row>
-    </Container>
-  </>
-);
+handleChange(index, event) {
+  const { adminChecked } = this.state;
+  let newArr = adminChecked;
+  newArr[index] = !newArr[index]
+  this.setState({adminChecked: newArr})
+};
+
+renderMembers() {
+  const { adminChecked } = this.state;
+  return (
+    adminChecked.length > 0 ? 
+    <>
+      <Grid container spacing={5}>
+        {members.map((user, index) => {
+          return (
+            <Grid className="user-card-grid" key={index} item xs={2}>
+              <Card key={index} variant="outlined">
+              <CardHeader
+              title={user.name}
+              />
+              <CardContent>
+                Toggle Admin? 
+                <Switch
+                  checked={adminChecked[index]}
+                  onChange={(event) => this.handleChange(index, event)}
+                  color="primary"
+                />
+              </CardContent>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </>
+    : null
+  )
+}
+
+  render() {
+    return(
+      <>
+        <div>
+        <h1 style={headerStyle}>Team Management</h1>
+          <Container fluid>
+            <Row>
+              <Col xs={6} md={2}>
+                <h2>Team Controls</h2>
+                <ul>
+                <li>Create Team</li>
+                <li>Edit Team</li>
+                </ul>
+              </Col>
+              <Col xs={12} md={10}>
+                <h2>Members</h2>
+                {this.renderMembers()}
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </>
+    )
+  };
+}
 
 export default TeamManagement;
