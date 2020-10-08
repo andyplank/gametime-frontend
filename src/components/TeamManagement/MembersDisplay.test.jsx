0bfed405e6e0c267d7552f37acc3065f35f140da
Collin <collin.li0499@gmail.com>
2020-10-08 16:53:23 -0400

    test things

--- src/components/TeamManagement/MembersDisplay.test.jsx
+++ src/components/TeamManagement/MembersDisplay.test.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import Switch from '@material-ui/core/Switch';
 import MembersDisplay from './MembersDisplay';
 
 Enzyme.configure({ adapter: new Adapter() });
@@ -12,22 +13,20 @@ describe('<MembersDisplay />', () => {
     { userID: 1, firstName: 'Raymond', lastName: 'Truong' },
     { userID: 1, firstName: 'Andy', lastName: 'Plank' },
   ];
-  const wrapper = Enzyme.shallow(
+  const wrapper = Enzyme.mount(
     <MembersDisplay members={testMembers} />,
   );
 
-  it('clicking item should set to on', () => {
+  it('clicking item should call the handler', () => {
     wrapper.instance().handleAdminChange = jest.fn();
     wrapper.instance().forceUpdate();
     wrapper.update();
-    wrapper.find('.adminSwitch').at(0).simulate('change', { target: { value: true } });
+    wrapper.find(Switch).at(0).simulate('change', { target: { checked: true } });
     expect(wrapper.instance().handleAdminChange).toHaveBeenCalled();
   });
 
   it('item should be on when clicked', () => {
-    wrapper.instance().forceUpdate();
-    wrapper.update();
-    const item = wrapper.find('.adminSwitch').at(0);
-    expect(item.prop('checked')).toBe(true);
+    const item = wrapper.find(Switch).at(0);
+    expect(item('checkbox')).to.have.property('checked', true);
   });
 });
