0956c0cce814f68b8c39595be7f92d6a6bcfdaf8
Trevor <trevor@hilldaddy.com>
2020-10-08 22:48:47 -0400

    added team management linking to header menu

--- src/components/TeamManagement/TeamManagement.jsx
+++ src/components/TeamManagement/TeamManagement.jsx
@@ -22,11 +22,8 @@ import CloseIcon from '@material-ui/icons/Close';
 import Modal from 'react-bootstrap/Modal';
 import TextField from '@material-ui/core/TextField';
 import PlayersDisplay from './PlayersDisplay';
-import {
-  Container,
-  Row,
-  Col,
-} from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+import Header from '../Header/Header';
 
 const players = [
   { id: 1, firstName: 'Daniel', lastName: 'Plue' },
@@ -47,10 +44,19 @@ const headerStyle = {
   paddingBottom: '3%',
 };
 
-class TeamManagement extends React.Component {
+const TeamManagement = () => {
+  return (
+    <>
+      <Header />
+      <Content />
+    </>
+  );
+};
+
+class Content extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       adminChecked: [],
       showTeamEdit: false,
       showTeamCreate: false,
@@ -60,66 +66,60 @@ class TeamManagement extends React.Component {
       fundDescError: false,
       accountNumberError: false,
       routingNumberError: false,
-      teamName: "",
+      teamName: '',
     };
   }
 
+  validateFields(team) {
+    let canSave = true;
 
-validateFields(team) {
-  let canSave = true;
+    if (team.name.length > 60) {
+      canSave = false;
+      this.setState({ teamNameError: true });
+    } else {
+      this.setState({ teamNameError: false });
+    }
 
-  if(team.name.length > 60){
-    canSave = false;
-    this.setState({ teamNameError: true });
-  }
-  else{
-    this.setState({ teamNameError: false })
+    if (isNaN(team.fundGoal)) {
+      canSave = false;
+      this.setState({ fundGoalError: true });
+    } else {
+      this.setState({ fundGoalError: false });
+    }
+
+    if (team.fundDesc.length > 100) {
+      canSave = false;
+      this.setState({ fundDescError: true });
+    } else {
+      this.setState({ fundDescError: false });
+    }
+
+    if (team.accountNumber > 12) {
+      canSave = false;
+      this.setState({ accountNumberError: true });
+    } else {
+      this.setState({ accountNumberError: false });
+    }
+
+    if (team.routingNumber > 9 || team.routingNumber < 9) {
+      canSave = false;
+      this.setState({ routingNumberError: true });
+    } else {
+      this.setState({ routingNumberError: false });
+    }
   }
 
-  if(isNaN(team.fundGoal)){
-    canSave = false;
-    this.setState({ fundGoalError: true });
-  }
-  else{
-    this.setState({ fundGoalError: false })
+  handleTeamEditClick() {
+    //TODO
+    this.setState({ showTeamEdit: true });
+    console.log('edit clicked');
   }
 
-  if(team.fundDesc.length > 100){
-    canSave = false;
-    this.setState({ fundDescError: true });
+  handleTeamCreateClick() {
+    //TOD
+    this.setState({ showTeamCreate: true });
+    console.log('create clicked');
   }
-  else{
-    this.setState({ fundDescError: false })
-  }
-
-  if(team.accountNumber > 12){
-    canSave = false;
-    this.setState({ accountNumberError: true });
-  }
-  else{
-    this.setState({ accountNumberError: false })
-  }
-
-  if(team.routingNumber > 9 || team.routingNumber < 9){
-    canSave = false;
-    this.setState({ routingNumberError: true });
-  }
-  else{
-    this.setState({ routingNumberError: false })
-  }
-}
-
-handleTeamEditClick(){
-  //TODO
-  this.setState({ showTeamEdit: true })
-  console.log("edit clicked")
-}
-
-handleTeamCreateClick(){
-  //TOD
-  this.setState({ showTeamCreate: true })
-  console.log("create clicked")
-}
 
   handleTeamInviteClick() {
     //TODO generate link with endpoint
@@ -128,21 +128,21 @@ handleTeamCreateClick(){
 
   handleEditClose() {
     //TODO clear any edits
-    this.setState({ showTeamEdit: false })
+    this.setState({ showTeamEdit: false });
   }
 
   handleSaveTeamEdits() {
     //TODO call endpoint(PUT) to save edits to team
-    console.log("save team edits clicked")
+    console.log('save team edits clicked');
   }
 
   handleSaveTeamCreate() {
     //TODO call endpoint(POST) to create team
-    console.log("saving team: " + this.state.teamName)
+    console.log('saving team: ' + this.state.teamName);
   }
 
   renderTeamEditModal() {
-    const { 
+    const {
       showTeamEdit,
       teamNameError,
       fundGoalError,
@@ -151,45 +151,71 @@ handleTeamCreateClick(){
       routingNumberError,
     } = this.state;
     return (
-      <Modal
-        show={showTeamEdit}
-        onHide={() => this.handleEditClose()}
-      >
+      <Modal show={showTeamEdit} onHide={() => this.handleEditClose()}>
         <Modal.Header closeButton>
           <Modal.Title>Edit Team</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <form>
-            <div style={{ paddingBottom: "5%" }}>
-              <TextField variant="outlined" label="Team Name" error={teamNameError}/>
+            <div style={{ paddingBottom: '5%' }}>
+              <TextField
+                variant="outlined"
+                label="Team Name"
+                error={teamNameError}
+              />
             </div>
-            <div style={{ paddingBottom: "5%" }}>
-              <TextField variant="outlined" label="Fund Goal" error={fundGoalError} />
+            <div style={{ paddingBottom: '5%' }}>
+              <TextField
+                variant="outlined"
+                label="Fund Goal"
+                error={fundGoalError}
+              />
             </div>
-            <div style={{ paddingBottom: "5%" }}>
-              <TextField variant="outlined" label="Fund Description" error={fundDescError} />
+            <div style={{ paddingBottom: '5%' }}>
+              <TextField
+                variant="outlined"
+                label="Fund Description"
+                error={fundDescError}
+              />
             </div>
-            <div style={{ paddingBottom: "5%" }}>
-              <TextField variant="outlined" label="Account Number" error={accountNumberError}/>
+            <div style={{ paddingBottom: '5%' }}>
+              <TextField
+                variant="outlined"
+                label="Account Number"
+                error={accountNumberError}
+              />
             </div>
             <div>
-              <TextField variant="outlined" label="Routing Number" error={routingNumberError}/>
+              <TextField
+                variant="outlined"
+                label="Routing Number"
+                error={routingNumberError}
+              />
             </div>
-            </form>
+          </form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="contained" color="secondary" onClick={() => this.handleEditClose()}>Close</Button>
-          <Button variant="contained" color="primary" onClick={() => this.handleSaveTeamEdits()}>Save changes</Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => this.handleEditClose()}
+          >
+            Close
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => this.handleSaveTeamEdits()}
+          >
+            Save changes
+          </Button>
         </Modal.Footer>
       </Modal>
-    )
+    );
   }
 
   renderTeamCreateModal() {
-    const { 
-      showTeamCreate,
-      teamNameError,
-    } = this.state;
+    const { showTeamCreate, teamNameError } = this.state;
     return (
       <Modal
         show={showTeamCreate}
@@ -200,17 +226,36 @@ handleTeamCreateClick(){
         </Modal.Header>
         <Modal.Body>
           <form>
-            <div style={{ paddingBottom: "5%" }}>
-              <TextField variant="outlined" label="Team Name" error={teamNameError} onChange={(event) => this.setState({ teamName: event.target.value })}/>
+            <div style={{ paddingBottom: '5%' }}>
+              <TextField
+                variant="outlined"
+                label="Team Name"
+                error={teamNameError}
+                onChange={(event) =>
+                  this.setState({ teamName: event.target.value })
+                }
+              />
             </div>
           </form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="contained" color="secondary" onClick={() => this.setState({ showTeamCreate: false })}>Close</Button>
-          <Button variant="contained" color="primary" onClick={() => this.handleSaveTeamCreate()}>Create</Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => this.setState({ showTeamCreate: false })}
+          >
+            Close
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => this.handleSaveTeamCreate()}
+          >
+            Create
+          </Button>
         </Modal.Footer>
       </Modal>
-    )
+    );
   }
 
   renderInviteLinkModal() {
@@ -227,12 +272,12 @@ handleTeamCreateClick(){
           <p style={{ textAlign: 'center' }}>invite link here</p>
         </Modal.Body>
       </Modal>
-    )
+    );
   }
 
   render() {
-    return(
-      <div style={{ height: "100%" }}>
+    return (
+      <div style={{ height: '100%' }}>
         {this.renderTeamEditModal()}
         {this.renderTeamCreateModal()}
         {this.renderInviteLinkModal()}
@@ -240,11 +285,13 @@ handleTeamCreateClick(){
         <Container fluid>
           <Row>
             <Col xs={6} md={2}>
-              <h2 style={{ display:'inline-block', paddingBottom: '2%' }}>Team Control</h2>
+              <h2 style={{ display: 'inline-block', paddingBottom: '2%' }}>
+                Team Control
+              </h2>
               <p>
-                <Button 
-                  className="btn-team" 
-                  variant="contained" 
+                <Button
+                  className="btn-team"
+                  variant="contained"
                   color="primary"
                   onClick={() => this.handleTeamEditClick()}
                 >
@@ -252,9 +299,9 @@ handleTeamCreateClick(){
                 </Button>
               </p>
               <p>
-                <Button 
+                <Button
                   className="btn-team"
-                  variant="contained" 
+                  variant="contained"
                   color="primary"
                   onClick={() => this.handleTeamInviteClick()}
                 >
@@ -262,11 +309,11 @@ handleTeamCreateClick(){
                 </Button>
               </p>
               <p>
-                <Button 
+                <Button
                   className="btn-team"
-                  variant="contained" 
+                  variant="contained"
                   color="primary"
-                  startIcon={ <AddCircleOutlineIcon/> }
+                  startIcon={<AddCircleOutlineIcon />}
                   onClick={() => this.handleTeamCreateClick()}
                 >
                   Create Team
@@ -277,14 +324,14 @@ handleTeamCreateClick(){
             <Col xs={12} md={10}>
               <div style={{ paddingLeft: '5%' }}>
                 <h2>Players</h2>
-                <PlayersDisplay players={players}/>
+                <PlayersDisplay players={players} />
               </div>
             </Col>
           </Row>
         </Container>
       </div>
-    )
-  };
+    );
+  }
 }
 
 export default TeamManagement;
