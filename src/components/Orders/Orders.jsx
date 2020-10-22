70ec17ddd48726aa56b9195a1d32d748d1339b86
Collin <collin.li0499@gmail.com>
2020-10-22 19:28:54 -0400

    displaying orders now (using mock data)

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,6 +1,67 @@
 /* eslint-disable */
 import React, { useEffect, useState } from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import IconButton from '@material-ui/core/IconButton';
 import { useSelector } from 'react-redux';
+import Collapse from '@material-ui/core/Collapse';
+
+const mockData = [
+	{
+		order_id: "ABC",
+		status: 1,
+		buyer_email: 'plank2@purdue.edu',
+		buyer_address: '1301 3rd St',
+		items: [
+			{
+				item_id: '1234',
+				quantity: 4,
+				type: 'Small'
+			},
+			{
+				item_id: '12',
+				quantity: 1,
+				type: 'Large'
+			},
+			{
+				item_id: '4567',
+				quantity: 2,
+				type: ''
+			}
+		]
+	},
+	{
+		order_id: "def",
+		status: 1,
+		buyer_email: 'li2718@purdue.edu',
+		buyer_address: '1225 West State Street',
+		items: [
+			{
+				item_id: '1',
+				quantity: 4,
+				type: 'Small'
+			},
+			{
+				item_id: '2',
+				quantity: 1,
+				type: 'Large'
+			},
+			{
+				item_id: '3',
+				quantity: 2,
+				type: ''
+			}
+		]
+	},
+
+]
 
 const Orders = (props) => {
 	function selector(store) {
@@ -13,19 +74,81 @@ const Orders = (props) => {
 	}	
 
 	const [orders, setOrders] = useState([]);
-
 	const state = useSelector(selector);
 
 	useEffect(() => {
 		const orders = [] // call api to retrieve orders
 		setOrders(orders);
 	}, []);
+
+	function Row(props) {
+		const { row } = props;
+		const [open, setOpen] = React.useState(false);
+	  
+		return (
+		  <React.Fragment>
+			<TableRow>
+			  <TableCell>
+				<IconButton style={{outline: "none"}} aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
+				  {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+				</IconButton>
+			  </TableCell>
+			  <TableCell component="th" scope="row">
+				{row.status}
+			  </TableCell>
+			  <TableCell align="right">{row.buyer_email}</TableCell>
+			  <TableCell align="right">{row.buyer_address}</TableCell>
+			</TableRow>
+			<TableRow>
+			  <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+			  <Collapse in={open} timeout="auto" unmountOnExit>
+				<Table size="small" aria-label="Items">
+					<TableHead>
+						<TableRow>
+							<TableCell>item_id</TableCell>
+							<TableCell align="right">quantity</TableCell>
+							<TableCell align="right">type</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{row.items.map((item) => (
+							<TableRow key={item.item_id}>
+								<TableCell>{item.item_id}</TableCell>
+								<TableCell align="right">{item.quantity}</TableCell>
+								<TableCell align="right">{item.type.length > 0 ? item.type : "N/A"}</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</Collapse>
+			</TableCell>
+			</TableRow>
+		  </React.Fragment>
+		);
+	  }
     
 	return (
 		<>
-			<p>
-				List of Orders - will have a dropdown next to each one representing order status
-			</p>
+			{mockData.map((row) => (
+				<div style={{width: "50%", paddingBottom: "3%"}}>
+					<TableContainer component={Paper}>
+					<Table aria-label="simple table" size="small">
+						<TableHead>
+						<TableRow>
+							<TableCell/>
+							<TableCell>Status</TableCell>
+							<TableCell align="right">Buyer's&nbsp;Email</TableCell>
+							<TableCell align="right">Address</TableCell>
+							<TableCell align="right">Items</TableCell>
+						</TableRow>
+						</TableHead>
+						<TableBody>
+							<Row key={row.order_id} row={row}/>
+						</TableBody>
+					</Table>
+				</TableContainer>
+			</div>
+			))}
 		</>
 	)   
     
--- src/utils/team/team.js
+++ src/utils/team/team.js
@@ -1,11 +1,9 @@
-import axios from 'axios';
-
-const auth = 'eyJraWQiOiJmUk1xT1J0SFNTcnk3RWNsYmtQbXlVK1wvZlNabjhWOElWWU5vY3A0K3Ricz0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI4ZjVjOTZlOS0xZjJlLTQ3MjItOGRjOS04ODM5NWFmNTJlYTUiLCJldmVudF9pZCI6IjFlZGY0MmNkLTZiMjMtNDdjNC05NzIzLTYzOTc0M2Y0MWE3YyIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE2MDIyMTk2MDQsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy1lYXN0LTIuYW1hem9uYXdzLmNvbVwvdXMtZWFzdC0yX1cwRXRtZHg0cyIsImV4cCI6MTYwMjMwNjAwNCwiaWF0IjoxNjAyMjE5NjA1LCJqdGkiOiIzNDNlMzMwMC00MzgxLTQxY2EtYWFkZC01MzNhZmQxNjAwZjIiLCJjbGllbnRfaWQiOiI3dWQxcXJ1OGV1Mm8xdmh0Nm9vNnBodXRudSIsInVzZXJuYW1lIjoiOGY1Yzk2ZTktMWYyZS00NzIyLThkYzktODgzOTVhZjUyZWE1In0.euLFGXaJc8FVMXMq7uB0XoywbPmDZSnxZup1WWzhMKrO_89UUD5AgNAthQKuuvyTWJ4fx4lEQWfrf_xWt6yWHs9eaXSdApqvO4OozpActgjBqzoWfISObtlOFgt8vADFqjOoQn8OoQx9GJGQhcsNuTou9YTv_wJLZkQ4MrF3KOjVR1cjRZpmlYSMPRIon5qajyvF3Kymhb6riT-XoIlxHxLLjGbzRWAAkqQ0s68Pkbp2Mu1mpVG2G9jK5pli8uhrCWCTrHZ29dOnl8DQLbQwVNL-tex5r_K20WJZ4OMqZ2t7PN6uTM9coWpUmm4Xs7ZnYt9l-BOH3-mwOKddWQJibQ';
+import networker from '../networker/networker';
 
 export async function getTeamData(teamId) {
     const headers = {
-        'Content-Type': 'application/json',
-        'Authorization': auth    }
+        'Content-Type': 'application/json'
+    }
 
     const data = {
         team: teamId
@@ -18,7 +16,7 @@ export async function getTeamData(teamId) {
         data: data
     }
 
-    const res = await axios(config);
+    const res = await networker(config);
     console.log("team data", res);
     return res;
 }
@@ -26,7 +24,7 @@ export async function getTeamData(teamId) {
 export async function getTeamsForUser(userId) {
     const headers = {
         'Content-Type': 'application/json',
-        'Authorization': auth    }
+    }
 
     const config = {
         method: 'get',
@@ -34,7 +32,7 @@ export async function getTeamsForUser(userId) {
         headers: headers,
     }
 
-    const res = await axios(config);
+    const res = await networker(config);
     console.log("teams", res.data.teams)
 
     return res.data.teams;
@@ -42,8 +40,8 @@ export async function getTeamsForUser(userId) {
 
 export async function createTeam(ownerId, name) {
     const headers = {
-        'Content-Type': 'application/json',
-        'Authorization': auth    }
+        'Content-Type': 'application/json'
+    }
 
     const data = {
         name: name,
@@ -57,7 +55,7 @@ export async function createTeam(ownerId, name) {
         data: data
     }
 
-    const res = await axios(config);
+    const res = await networker(config);
 
     console.log(res);
 }
@@ -65,7 +63,7 @@ export async function createTeam(ownerId, name) {
 export async function editTeam(teamId, name) {
     const headers = {
         'Content-Type': 'application/json',
-        'Authorization': auth    }
+    }
 
     const data = {
         name: name,
@@ -79,7 +77,7 @@ export async function editTeam(teamId, name) {
         data: data
     }
 
-    const res = await axios(config);
+    const res = await networker(config);
 
     console.log("edit team", res);
 }
@@ -87,7 +85,7 @@ export async function editTeam(teamId, name) {
 export async function joinTeam(teamId, userId) {
     const headers = {
         'Content-Type': 'application/json',
-        'Authorization': auth    }
+    }
 
     const data = {
         user: userId
@@ -100,7 +98,7 @@ export async function joinTeam(teamId, userId) {
         data: data
     }
 
-    const res = await axios(config);
+    const res = await networker(config);
 
     console.log(res);
 }
@@ -108,7 +106,7 @@ export async function joinTeam(teamId, userId) {
 export async function removeFromTeam(teamId, userId) {
     const headers = {
         'Content-Type': 'application/json',
-        'Authorization': auth    }
+    }
 
     const data = {
         team: teamId,
@@ -122,7 +120,7 @@ export async function removeFromTeam(teamId, userId) {
         data: data
     }
 
-    const res = await axios(config);
+    const res = await networker(config);
 
     console.log(res);
 }
@@ -130,7 +128,7 @@ export async function removeFromTeam(teamId, userId) {
 export async function editPermission(teamId, userId, permission) {
     const headers = {
         'Content-Type': 'application/json',
-        'Authorization': auth    }
+    }
 
     const data = {
         team: teamId,
@@ -145,7 +143,7 @@ export async function editPermission(teamId, userId, permission) {
         data: data
     }
 
-    const res = await axios(config);
+    const res = await networker(config);
 
     console.log(res);
 }
