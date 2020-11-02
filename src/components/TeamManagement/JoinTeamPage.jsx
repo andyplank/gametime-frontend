6077914428158681ab6587040be76682dcc0e637
Collin <collin.li0499@gmail.com>
2020-11-02 17:21:03 -0500

    added teamid to route params

--- src/components/TeamManagement/JoinTeamPage.jsx
+++ src/components/TeamManagement/JoinTeamPage.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useSelector} from 'react-redux';
 import {Redirect} from 'react-router-dom';
 import {joinTeam} from "../../utils/team/team";
@@ -17,18 +17,20 @@ const JoinTeamPage = () => {
     
     const state = useSelector(selector);
 
+    const [team, setTeam] = useState(null);
+
     useEffect(() => {
         const join = async () => {
           let teamId = "";
           teamId += window.location.href;
           teamId = teamId.substr(teamId.lastIndexOf("/")+1);
-          console.log(teamId);
+          setTeam(teamId);
           await joinTeam(teamId);
         }
         join();
       },[]);
 
-    return state.signed_in && <Redirect to={{ pathname: '/home' }} />
+    return (state.signed_in && team) && <Redirect to={{ pathname: `/${team}/home` }} />
 };
 
 
