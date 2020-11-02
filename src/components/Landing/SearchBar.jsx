e18322502aae4452027e96fae281fc4cf7a11784
Andy Plank <aplank14@gmail.com>
2020-11-02 14:25:35 -0500

    Bug fix on search bar

--- src/components/Landing/SearchBar.jsx
+++ src/components/Landing/SearchBar.jsx
@@ -1,8 +1,9 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
 
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
-import {Button, InputGroup} from 'react-bootstrap';
+import { InputGroup} from 'react-bootstrap';
 
 import CircularProgress from '@material-ui/core/CircularProgress';
 
@@ -12,71 +13,84 @@ function sleep(delay = 0) {
   });
 }
 const SearchBar = () => {
-    const [open, setOpen] = React.useState(false);
-    const [options, setOptions] = React.useState([]);
-    const loading = open && options.length === 0;
-  
-    const refresh = () => {
-      (async () => {
-        await sleep(10000); // For demo purposes.
-        const countries = [
-          { id: '1234', label: 'San Marcos Lacrosse' },
-          { id: '4567', label: 'LaPorte Badmitten' }
-        ]; 
-  
-        setOptions(countries);
-      })();
-    }
-  
-    useEffect(() => {
-      refresh();
-    }, [])
+  const [value, setValue] = useState(null);  
+  const [inputValue, setInputValue] = useState('');
+  const [teamId, setTeamID] = useState('');  
+  const [open, setOpen] = useState(false);
 
-    return (
-      <InputGroup className="d-flex">
-        <Autocomplete
-          id="team-search-bar"
-          className="grow"
-          open={open}
-          onOpen={() => {
-                        setOpen(true);
-                    }}
-          onClose={() => {
-                        setOpen(false);
-                    }}
-          getOptionLabel={(option) => option.label}
-          options={options}
-          loading={loading}
-          renderOption={(option) => (
-            <>
-              {option.label}
-            </>
-                    )}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              label="Search for your team"
-              variant="outlined"
-              InputProps={{
-                    ...params.InputProps,
-                    className:"search-bar",
-                    endAdornment: (
-                      <>
-                        {loading ? <span className="pr-3"><CircularProgress color="inherit" size={20} /></span> : null}
-                        {params.InputProps.endAdornment}
-                      </>
-                    ),
-                    }}
-            />
-            )}
-        />
-        <InputGroup.Append>
-          <Button variant="outline-primary" className="search-button">
-            {' Go '}
-          </Button>
-        </InputGroup.Append>
-      </InputGroup>
-    );
+  const [options, setOptions] = useState([]);
+  const loading = open && options.length === 0;
+
+  const refresh = () => {
+    (async () => {
+      await sleep(100); // For demo purposes.
+      const countries = [
+        { id: '1234', label: 'San Marcos Lacrosse' },
+        { id: '4567', label: 'LaPorte Badmitten' }
+      ]; 
+      setOptions(countries);
+    })();
+  }
+
+  useEffect(() => {
+    refresh();
+  }, [])
+
+  return (
+    <InputGroup className="d-flex">
+      <Autocomplete
+        id="team-search-bar"
+        className="grow"
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+          if(newValue && newValue.id !== undefined){
+            setTeamID(newValue.id)
+          }
+        }}
+        inputValue={inputValue}
+        onInputChange={(event, newInputValue) => {
+          setInputValue(newInputValue);
+        }}
+        open={open}
+        onOpen={() => {
+          setOpen(true);
+        }}
+        onClose={() => {
+          setOpen(false);
+        }}
+        getOptionLabel={(option) => option.label !== undefined ? option.label : ''}
+        options={options}
+        loading={loading}
+        renderOption={(option) => (
+          <>
+            {option.label}
+          </>
+        )}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Search for your team"
+            variant="outlined"
+            InputProps={{
+              ...params.InputProps,
+              className:"search-bar",
+              endAdornment: (
+                <>
+                  {loading ? <span className="pr-3"><CircularProgress color="inherit" size={20} /></span> : null}
+                </>
+              ),
+            }}
+          />
+          )}
+      />
+      <InputGroup.Append>
+        <Link to={`${teamId}/home`} className="no-link btn btn-outline-primary d-flex align-items-center">
+          {' Go '}
+        </Link>
+      </InputGroup.Append>
+    </InputGroup>
+  );
 }
 
 export default SearchBar;
