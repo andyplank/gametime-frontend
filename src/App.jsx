e5849e44fbd7c7a42dfc2ae2d67c085695705bf2
Andy Plank <aplank14@gmail.com>
2020-11-02 14:00:58 -0500

    Changed color pallet

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 
@@ -15,16 +16,34 @@ import 'bootstrap/dist/css/bootstrap.css';
 const store = createStore(reducer);
 
 const App = () => {
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        light: '#0275d8',
+        main: '#0275d8',
+        dark: '#0275d8',
+        contrastText: '#fff',
+      },
+      secondary: {
+        light: '#0275d8',
+        main: '#0275d8',
+        dark: '#0275d8',
+        contrastText: '#000',
+      },
+    },
+  });
 
   return (
-    <Router>
-      <Switch>
-        <Route path="/login" exact component={Login} />
-        <Route path="/register" exact component={Register} />
+    <MuiThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route path="/login" exact component={Login} />
+          <Route path="/register" exact component={Register} />
         
-        <Routes />
-      </Switch>
-    </Router>
+          <Routes />
+        </Switch>
+      </Router>
+    </MuiThemeProvider>
   );
 };
 
