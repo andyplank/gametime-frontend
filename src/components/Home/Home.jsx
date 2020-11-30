1f34665b7093a17870d99bb18967dc095c1facef
Andy Plank <aplank14@gmail.com>
2020-11-30 15:46:17 -0500

    Improved home page and added banner viewing

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,49 +1,67 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
-import {Button, Container} from 'react-bootstrap';
+import {Button, Container, Carousel, Col, Row} from 'react-bootstrap';
 import './Home.scss';
 import { Link, useParams} from 'react-router-dom';
 import Contact from './Contact';
 
+import { fetchSponsorships } from '../../utils/business/business';
+
 const Home = () => {
 
   const { team_id } = useParams();
+  const [banners, setBanners] = useState([{sponsor_id: 1, link: 'https://i.imgur.com/KGK9Y2V.jpg'}, {sponsor_id: 2, link: 'https://i.imgur.com/KGK9Y2V.jpg'}]);
+  const [promotions, setPromotions] = useState(['https://i.imgur.com/KGK9Y2V.jpg', 'https://i.imgur.com/KGK9Y2V.jpg']);
 
-  function selector(store) {
-    return {
-      name:
-        store.user.teams.length > 0
-          ? store.user.teams[store.status.selected_team].name
-          : '',
-      role:
-        store.user.teams.length > 0
-          ? store.user.teams[store.status.selected_team].role
-          : '',
-    };
+  const refresh = async () => {
+    await fetchSponsorships(setBanners, team_id);
   }
 
-  // TODO: Use this
-  // eslint-disable-next-line no-unused-vars
-  const state = useSelector(selector);
+  useEffect(() => {
+    refresh();
+  }, [team_id])
+
+  const bannerContent = banners.map((elm, index) => {
+    return (
+      <Carousel.Item key={elm.sponsor_id}>
+        <img
+          className="d-block w-100"
+          src={elm.link}
+          alt={index}
+        />
+      </Carousel.Item>
+    );
+  });
 
   return (
     <div className="fill-vert">
+      <Carousel controls={false} indicators={false} interval={2000}>
+        {bannerContent}
+      </Carousel>
       <Jumbotron className="text-center">
         <h2>Team Page</h2>
       </Jumbotron>
       <Container>
 
-        <div className="py-2">
-          <Link to={`/team/${team_id}/store/`} className="no-link">
-            <Button>Go to team store</Button>
-          </Link>
-        </div>
-        <div className="py-2">
-          <Link to={`/team/${team_id}/fundraiser/`} className="no-link">
-            <Button>Go to team fundraiser</Button>
-          </Link>
-        </div>
+        <Row className="py-3">
+        
+          <Col md={6}>
+            <Link to={`/team/${team_id}/fundraiser/`} className="no-link">
+
+              <img src="https://www.halo.com/blog/wp-content/uploads/2020/01/pic-05-charity-1024x439.jpg" alt="Snow" className="w-100 grayscale rounded" />
+              <div className="centered text-white"><h3>Team Fundraiser</h3></div>
+            </Link>
+          </Col>
+
+          <Col md={6}>
+            <Link to={`/team/${team_id}/store/`} className="no-link">
+              <img src="https://www.halo.com/blog/wp-content/uploads/2020/01/pic-05-charity-1024x439.jpg" alt="Snow" className="w-100 grayscale rounded" />
+              <div className="centered text-white"><h3>Team Store</h3></div>
+            </Link>
+          </Col>
+        
+        </Row>
 
         <Contact />
 
